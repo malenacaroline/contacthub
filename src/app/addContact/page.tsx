a9a176047eae01b6bc76c464242df30c31f486d3
Malena Aguiar <malenaaguiar@Malenas-Mac-Mini.local>
2024-02-16 16:59:27 -0700

    initial project structure

---
+++ src/app/addContact/page.tsx
@@ -0,0 +1,5 @@
+import { Button } from "@chakra-ui/react";
+
+export default function AddContact() {
+  return <div><Button colorScheme='blue'>Add Contact</Button></div>;
+}
---
+++ src/app/editContact/page.tsx
@@ -0,0 +1,3 @@
+export default function EditContact() {
+  return <h1>Hello EditContact.</h1>;
+}
