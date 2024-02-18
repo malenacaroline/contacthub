991b290c3ff52ab8158c8d258638b2f28b7f9041
Malena Aguiar <malenaaguiar@Malenas-Mac-Mini.local>
2024-02-18 10:43:14 -0700

    added props with children type

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,16 +2,13 @@ import type { Metadata } from "next";
 import { Providers } from "./providers";
 import { Container } from "@chakra-ui/react";
 import { Navbar } from "./components/navbar";
+import { PropsWithChildren } from "react";
 
 export const metadata: Metadata = {
   title: "ContactHub",
 };
 
-export default function RootLayout({
-  children,
-}: Readonly<{
-  children: React.ReactNode;
-}>) {
+export default function RootLayout({ children }: PropsWithChildren) {
   return (
     <html lang="en">
       <body>
